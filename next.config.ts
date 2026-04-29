a7ff95cf4e85f25cffdb31da3b347025df3ad5f4
R4nz5r <ragibshahriar43@gmail.com>
2026-04-29 20:45:41 +0600

    increse file upload size

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100mb",
+    },
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "covers.openlibrary.org" },
